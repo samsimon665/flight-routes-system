da4aee8002e5e0a7c9ea43f4e2dd4d526e255c4c
samsimon665 <samsimon665@gmail.com>
2026-04-08 18:49:43 +0530

    Refactored templates using base template inheritance

---
+++ routes/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.add_route, name='add_route'),
+
+    path('nth/', views.nth_node_view, name='nth_node'),
+
+    path('longest/', views.longest_route_view, name='longest_route'),
+
+    path('shortest/', views.shortest_path_view, name='shortest_path'),
+]
